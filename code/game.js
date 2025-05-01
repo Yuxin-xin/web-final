3f0724c9d9a02503efb2d6d5e20f894084490ceb
Yuxin-xin <rzf509@york.ac.uk>
2025-05-01 14:51:22 +0100

    6

--- code/game.js
+++ code/game.js
@@ -1,7 +1,33 @@
 let score = 0;
 let timeLeft = 180;
+let gameActive = true;
 const gameArea = document.getElementById('game-area');
 const scoreDisplay = document.getElementById('score');
+const timerDisplay = document.getElementById('timer');
+
+//倒计时
+function startTimer() {
+    updateTimer(); // 立即更新显示
+    
+    const timer = setInterval(() => {
+        timeLeft--;
+        updateTimer();
+        
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            console.log("时间到！");
+        }
+    }, 1000);
+}
+
+function updateTimer() {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+startTimer();
+
 
 // 生成大鹅
 function createGoose() {
@@ -15,7 +41,7 @@ goose.style.bottom = '0px';
 goose.addEventListener('click', () => {
 goose.style.transform = 'scale(0)';
 score++;
-scoreDisplay.textContent = score;
+scoreDisplay.textContent = 'you got :'+score ;
 setTimeout(() => goose.remove(), 200);
 });
             
@@ -23,27 +49,17 @@ gameArea.appendChild(goose);
             
 // 自动移动
 let posY = 0;
-
 let velocityY = Math.random() * 5 + 15;
 const speedX = Math.random() * 3;
 const gravity = -1 * (Math.random() * 0.1 + 0.3);
 let posX = Math.random() * window.innerWidth * 0.7;
-// let velocity = -30;
 let horizontalPos = 0;
+
 const move = setInterval(() => {
-//velocity += gravity * -9.8;
-
-//pos += velocity * timeStep;
-
-//horizontalPos += speed;
 posX += speedX;
-goose.style.left = posX + 'px';
-//goose.style.left = horizontalPos + 'px';
-
-//goose.style.bottom = Math.max(0, pos) + 'px';
-velocityY += gravity;
 posY += velocityY;
-//pos += speed;
+goose.style.left = posX + 'px';
+velocityY += gravity;
 goose.style.bottom = posY + 'px';
                 
 // 移出屏幕后删除
@@ -59,6 +75,8 @@ goose.remove();
 }
 }, 20);
 }*/
+
+
 
 // 每1秒生成一只大鹅
 setInterval(createGoose, 1000);
